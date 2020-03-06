b4c5ecd26e3a60ec90de74f0d5a657842ba08da5
Gosow <yacine.alkassoum.etu@univ-lemans.fr>
2020-03-06 09:29:56 +0100

    fin fonctions d'aides

--- class/Aide.rb
+++ class/Aide.rb
@@ -70,98 +70,115 @@ class Aide
     end
    end
 
-   def testerEspace(unPlateau)
-    for i in range(unPlateau.taille)
-        for j in range(unPlateau.taille)
-            
-            if unPlateau.donneTaCaseInt(i,j) > 0
-
-                if unPlateau.coordValides(i+2,j)
-                    if unPlateau.donneTaCaseInt(i+2,j) > 0 && unPlateau.donneTaCaseInt(i+1,j) !=-1
-                        return true
-                    end
-                end
-                   
-
-                if unPlateau.coordValides(i,j+2)
-                    if unPlateau.donneTaCaseInt(i,j+2) >0 && unPlateau.donneTaCaseInt(i,j+1) !=-1
-                        return true
-                    end  
-                end
+   def Aide.testerEspace(unPlateau)
+        for i in range(unPlateau.taille)
+            for j in range(unPlateau.taille)
                 
+                if unPlateau.donneTaCaseInt(i,j) > 0
 
-                if unPlateau.coordValides(i,j-2)    
-                    if unPlateau.donneTaCaseInt(i,j-2) >0 && unPlateau.donneTaCaseInt(i,j-1) !=-1
-                        return true
-                    end
-                end
-                
-                
-                if unPlateau.coordValides(i-2,j)
-                    if unPlateau.donneTaCaseInt(i-2,j) > 0 && unPlateau.donneTaCaseInt(i-1,j) !=-1
-                        return true
-                    end
-                   
-                end
-
-               return false
-
-            end
-        end
-    end
-   end
-
-   def testerDiagonale(unPlateau)
-    for i in range(unPlateau.taille)
-        for j in range(unPlateau.taille)
-
-            if unPlateau.donneTaCaseInt(i,j) > 0
-
-                if unPlateau.coordValides(i+1,j+1)
-                    if unPlateau.donneTaCaseInt(i+1,j+1) > 0 
-                        if unPlateau.donneTaCaseInt(i+1,j) !=-1 || unPlateau.donneTaCaseInt(i,j+1) !=-1
-                            return true
-                        end
-                    end
-                end
-                   
-
-                if unPlateau.coordValides(i-1,j-1)
-                    if unPlateau.donneTaCaseInt(i-1,j-1) >0 
-                        if unPlateau.donneTaCaseInt(i-1,j) !=-1 || unPlateau.donneTaCaseInt(i,j-1) !=-1
+                    if unPlateau.coordValides(i+2,j)
+                        if unPlateau.donneTaCaseInt(i+2,j) > 0 && unPlateau.donneTaCaseInt(i+1,j) !=-1
                             return true
                         end
                     end
                     
-                end
-    
 
-                if unPlateau.coordValides(i+1,j-1)
-                    if unPlateau.donneTaCaseInt(i+1,j-1) >0
-                        if unPlateau.donneTaCaseInt(i+1,j) !=-1 || unPlateau.donneTaCaseInt(i,j-1) !=-1
+                    if unPlateau.coordValides(i,j+2)
+                        if unPlateau.donneTaCaseInt(i,j+2) >0 && unPlateau.donneTaCaseInt(i,j+1) !=-1
+                            return true
+                        end  
+                    end
+                    
+
+                    if unPlateau.coordValides(i,j-2)    
+                        if unPlateau.donneTaCaseInt(i,j-2) >0 && unPlateau.donneTaCaseInt(i,j-1) !=-1
                             return true
                         end
                     end
-                end
-                
-                
-                if unPlateau.coordValides(i-1,j+1)
-                    if unPlateau.donneTaCaseInt(i-1,j+1) > 0
-                        if unPlateau.donneTaCaseInt(i-1,j) !=-1 || unPlateau.donneTaCaseInt(i,j+1) !=-1
+                    
+                    
+                    if unPlateau.coordValides(i-2,j)
+                        if unPlateau.donneTaCaseInt(i-2,j) > 0 && unPlateau.donneTaCaseInt(i-1,j) !=-1
                             return true
                         end
-                    end
-                   
+                    end            
                 end
-               
-               return false
-
             end
         end
-    end
+        return false
    end
 
-   def testerBlancIsole(unPlateau)
-   end
+    def Aide.testerDiagonale(unPlateau)
+        for i in range(unPlateau.taille)
+            for j in range(unPlateau.taille)
+
+                if unPlateau.donneTaCaseInt(i,j) > 0
+
+                    if unPlateau.coordValides(i+1,j+1)
+                        if unPlateau.donneTaCaseInt(i+1,j+1) > 0 
+                            if unPlateau.donneTaCaseInt(i+1,j) !=-1 || unPlateau.donneTaCaseInt(i,j+1) !=-1
+                                return true
+                            end
+                        end
+                    end
+                    
+
+                    if unPlateau.coordValides(i-1,j-1)
+                        if unPlateau.donneTaCaseInt(i-1,j-1) >0 
+                            if unPlateau.donneTaCaseInt(i-1,j) !=-1 || unPlateau.donneTaCaseInt(i,j-1) !=-1
+                                return true
+                            end
+                        end
+                        
+                    end
+        
+
+                    if unPlateau.coordValides(i+1,j-1)
+                        if unPlateau.donneTaCaseInt(i+1,j-1) >0
+                            if unPlateau.donneTaCaseInt(i+1,j) !=-1 || unPlateau.donneTaCaseInt(i,j-1) !=-1
+                                return true
+                            end
+                        end
+                    end
+                    
+                    
+                    if unPlateau.coordValides(i-1,j+1)
+                        if unPlateau.donneTaCaseInt(i-1,j+1) > 0
+                            if unPlateau.donneTaCaseInt(i-1,j) !=-1 || unPlateau.donneTaCaseInt(i,j+1) !=-1
+                                return true
+                            end
+                        end
+                    
+                    end
+                
+                
+
+                end
+            end
+        end
+        return false
+    end
+
+    def Aide.testerBlancIsole(unPlateau)
+        for i in range(unPlateau.taille)
+            for j in range(unPlateau.taille)
+                
+                if unPlateau.donneTaCaseInt(i,j) == 0
+
+                    if !unPlateau.coordValides(i+1,j) || unPlateau.donneTaCaseInt(i+1,j)==-1
+                        if !unPlateau.coordValides(i-1,j) || unPlateau.donneTaCaseInt(i-1,j)==-1
+                            if !unPlateau.coordValides(i,j+1) || unPlateau.donneTaCaseInt(i,j+1)==-1
+                                if !unPlateau.coordValides(i,j-1) || unPlateau.donneTaCaseInt(i,j-1)==-1
+                                    return true
+                                end
+                            end
+                        end
+                    end
+                
+                end
+            end
+        end
+        return false
+    end
 
 end
